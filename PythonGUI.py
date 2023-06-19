8d593df6eb71003adac71f5564ef02f840719d9e
IkaikaLee <leeikaika4@gmail.com>
2023-06-19 00:21:12 -0500

    some formatting

--- PythonGUI.py
+++ PythonGUI.py
@@ -32,41 +32,45 @@ def load_supervised_model_output():
 
 window = Tk()
 
-# Open window having dimension 100x100
 
-window.geometry('750x700')
+window.geometry('1200x300')
+leftFrame = Frame(window)
+leftFrame.pack(side=LEFT, anchor='center', fill='both')
 
+rightFrame = Frame(window)
+rightFrame.pack(side=RIGHT, anchor='center', fill='both')
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input the File Location for GMM", font="Courier 14 bold")
+label = Label(leftFrame, text="Input the File Location for GMM", font="Courier 14 bold")
+
 label.pack()
 
 
 # Create an Entry widget to accept User Input
 
-fileEntry = Entry(window, width=40)
+fileEntry = Entry(leftFrame, width=40)
 fileEntry.focus_set()
 fileEntry.pack()
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input Save Location for GMM Output", font="Courier 14 bold")
+label = Label(leftFrame, text="Input Save Location for GMM Output", font="Courier 14 bold")
 label.pack()
 # Create an Entry widget to accept User Input
 
-savePathEntry = Entry(window, width=40)
+savePathEntry = Entry(leftFrame, width=40)
 savePathEntry.focus_set()
 savePathEntry.pack()
 
 # Create a Button to load file path
 
-ttk.Button(window, text="Run GMM", width=20, command=load_gmm).pack(pady=20)
+ttk.Button(leftFrame, text="Run GMM", width=20, command=load_gmm).pack(pady=20)
 
 # Initialize a Label to display the User Input
 
 label = Label(window, text="Input Location of GMM Output", font="Courier 14 bold")
-label.pack()
+label.pack(pady=5)
 # Create an Entry widget to accept User Input
 
 gmmOutputEntry = Entry(window, width=40)
@@ -79,47 +83,47 @@ ttk.Button(window, text="Run RF Supervised Model", width=20, command=load_superv
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input Location of Supervised Model Output", font="Courier 14 bold")
+label = Label(rightFrame, text="Input Location of Supervised Model Output", font="Courier 14 bold")
 label.pack()
 # Create an Entry widget to accept User Input
 
-supervisedModelLocationEntry = Entry(window, width=40)
+supervisedModelLocationEntry = Entry(rightFrame, width=40)
 supervisedModelLocationEntry.focus_set()
 supervisedModelLocationEntry.pack()
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input Location of Masked Files", font="Courier 14 bold")
+label = Label(rightFrame, text="Input Location of Masked Files", font="Courier 14 bold")
 label.pack()
 # Create an Entry widget to accept User Input
 
-maskedFilesEntry = Entry(window, width=40)
+maskedFilesEntry = Entry(rightFrame, width=40)
 maskedFilesEntry.focus_set()
 maskedFilesEntry.pack()
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input Location of Segmented Files", font="Courier 14 bold")
+label = Label(rightFrame, text="Input Location of Segmented Files", font="Courier 14 bold")
 label.pack()
 # Create an Entry widget to accept User Input
 
-segmentedFilesEntry = Entry(window, width=40)
+segmentedFilesEntry = Entry(rightFrame, width=40)
 segmentedFilesEntry.focus_set()
 segmentedFilesEntry.pack()
 
 # Initialize a Label to display the User Input
 
-label = Label(window, text="Input Location of Core 300um Files", font="Courier 14 bold")
+label = Label(rightFrame, text="Input Location of Core 300um Files", font="Courier 14 bold")
 label.pack()
 # Create an Entry widget to accept User Input
 
-core300UmFilesEntry = Entry(window, width=40)
+core300UmFilesEntry = Entry(rightFrame, width=40)
 core300UmFilesEntry.focus_set()
 core300UmFilesEntry.pack()
 
 # Create a Button to load file path
 
-ttk.Button(window, text="Run RF Predict", width=20, command=load_supervised_model_output).pack(pady=20)
+ttk.Button(rightFrame, text="Run RF Predict", width=20, command=load_supervised_model_output).pack(pady=20)
 
 
 window.mainloop()
